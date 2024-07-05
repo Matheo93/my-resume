781ecd527f8f1c79b13a0705a6ec49a116774422
Matheo93 <Math.be2015@gmail.com>
2024-07-05 16:26:48 +0200

    Integrate MyResume HTML and assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Experience from './components/Experience';
-import Footer from './components/Footer';
-import './styles/styles.css';
+import React from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import "./styles/styles.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={() => (window.location.href = "/index.html")}
+        />
+        <Route
+          path="/portfolio-details"
+          component={() => (window.location.href = "/portfolio-details.html")}
+        />
+      </Switch>
+    </Router>
+  );
+}
 
 function App() {
   return (
