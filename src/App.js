2398f23a42a846029dc6b44269193845f236fc6d
Matheo93 <Math.be2015@gmail.com>
2024-07-05 16:32:17 +0200

    Fix identifier conflict by renaming App to MyApp

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
-import React from "react";
-import Header from "./components/Header";
-import About from "./components/About";
-import Experience from "./components/Experience";
-import Footer from "./components/Footer";
-import "./styles/styles.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import About from './components/About';
+import Experience from './components/Experience';
+import Footer from './components/Footer';
+import './styles/styles.css';
 
-function App() {
+// Renommez cette fonction pour éviter les conflits
+function MyApp() {
   return (
     <Router>
       <Switch>
-        <Route
-          exact
-          path="/"
-          component={() => (window.location.href = "/index.html")}
-        />
-        <Route
-          path="/portfolio-details"
-          component={() => (window.location.href = "/portfolio-details.html")}
-        />
+        <Route exact path="/" component={() => window.location.href = '/index.html'} />
+        <Route path="/portfolio-details" component={() => window.location.href = '/portfolio-details.html'} />
       </Switch>
     </Router>
   );
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <main>
-        <About />
-        <Experience />
-      </main>
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
+export default MyApp;
